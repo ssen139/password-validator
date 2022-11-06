e53d4ebfc0e13bffa45036d78db5381bea0861b9
Sourav Sen <ssen@yodlee.com>
2022-11-06 13:24:04 +0530

    creating PasswordValidator class with password length validation

---
+++ src/main/java/dev/ssen/password/validator/service/PasswordValidator.java
@@ -0,0 +1,32 @@
+package dev.ssen.password.validator.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author ssen
+ *
+ */
+public class PasswordValidator {
+	private List<String> messages;
+
+	public PasswordValidator() {
+		messages = new ArrayList<String>();
+	}
+
+	public boolean validate(String password) {
+		boolean isValid = true;
+
+		if (password.length() < 8) {
+			isValid = false;
+			messages.add("Password length should be more than 8 chars");
+		}
+
+		return isValid;
+	}
+
+	public List<String> getMessages() {
+		return messages;
+	}
+}

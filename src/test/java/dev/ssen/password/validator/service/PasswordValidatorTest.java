e0a0c5e86440237c1cc39e0b5050dbc3e0f90c96
Sourav Sen <ssen@yodlee.com>
2022-11-06 13:40:51 +0530

    adding test cases for null/empty validation. moving init of passwordValidator to setUp method

--- src/test/java/dev/ssen/password/validator/service/PasswordValidatorTest.java
+++ src/test/java/dev/ssen/password/validator/service/PasswordValidatorTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -14,19 +15,43 @@ import org.junit.Test;
  *
  */
 public class PasswordValidatorTest {
+	
+	private PasswordValidator passwordValidator;
+	
+	@Before
+	public void setUp() {
+		passwordValidator = new PasswordValidator();
+	}
 
 	@Test
 	public void testPasswordLengthInvalid() {
-		PasswordValidator passwordValidator = new PasswordValidator();
 		assertFalse(passwordValidator.validate("pass"));
 		assertThat(passwordValidator.getMessages(), hasItem("Password length should be more than 8 chars"));
 	}
 
 	@Test
 	public void testPasswordLengthValid() {
-		PasswordValidator passwordValidator = new PasswordValidator();
 		assertTrue(passwordValidator.validate("password"));
 		assertThat(passwordValidator.getMessages(), not(hasItem("Password length should be more than 8 chars")));
+	}
+	
+	@Test
+	public void testNullPasswordInvalid() {
+		assertFalse(passwordValidator.validate(null));
+		assertThat(passwordValidator.getMessages(), hasItem("Password should not be Empty"));
+	}
+	
+	@Test
+	public void testEmptyPasswordInvalid() {
+		assertFalse(passwordValidator.validate(""));
+		assertThat(passwordValidator.getMessages(), hasItem("Password should not be Empty"));
+		assertThat(passwordValidator.getMessages(), hasItem("Password length should be more than 8 chars"));
+	}
+	
+	@Test
+	public void testNonNullPasswordValid() {
+		passwordValidator.validate("pass");
+		assertThat(passwordValidator.getMessages(), not(hasItem("Password should not be Empty")));
 	}
 
 }
